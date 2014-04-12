39b06107bf1e9fee30b1f5fd9695a1c831e51962
lyeechong <shen@ubuntu.(none)>
2014-04-12 05:40:03 -0500

    .

---
+++ python/freeSpotCheckPrototype.py
@@ -0,0 +1,21 @@
+'''
+spots = {}
+spots[0] = 9
+spots[10] = 5
+spots[13] = 11
+spots[2] = 7
+spots[3] = 2
+
+yay = spots.keys()
+print yay
+yay.sort()
+print yay
+
+print yay[-1]
+'''
+
+spots = {}
+spots[1] = 3
+
+print 1 in spots.keys()
+print 2 in spots.keys()
